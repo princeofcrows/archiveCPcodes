32b32528c32d9efccb61f37c24f112f10d75ba63
prince_of_crows <tamjidjasir@gmail.com>
2022-10-06 22:18:55 +0600

    test

---
+++ cfCont/1735/cf1735a.cpp/b/b.cpp
@@ -0,0 +1,8 @@
+//
+//  b.cpp
+//  cf1735a.cpp
+//
+//  Created by Tamjid Jasir on 6/10/22.
+//
+
+#include "b.hpp"
---
+++ cfCont/1735/cf1735a.cpp/b/b.hpp
@@ -0,0 +1,13 @@
+//
+//  b.hpp
+//  cf1735a.cpp
+//
+//  Created by Tamjid Jasir on 6/10/22.
+//
+
+#ifndef b_hpp
+#define b_hpp
+
+#include <stdio.h>
+
+#endif /* b_hpp */
